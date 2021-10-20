e6890c4067b5d41ea79012a954c27f0dc4d964bb
SPP User <spp-user@spp-forum.de>
2021-10-20 11:35:36 +0300

    Часть 6 пр

--- WpfApp8/Pages/AddEditPage.xaml.cs
+++ WpfApp8/Pages/AddEditPage.xaml.cs
@@ -20,14 +20,48 @@ namespace WpfApp8.Pages
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Products _products = new Products();
+        public AddEditPage(Products SelectedProduct)
         {
             InitializeComponent();
+            if (SelectedProduct != null)
+                _products = SelectedProduct;
+            DataContext = _products;
         }
 
         private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
         {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_products.Product_name))
+                errors.AppendLine("Введите название продукта!");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_products.Date_of_receipt)))
+                errors.AppendLine("Введите дату!");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_products.Unit_price)))
+                errors.AppendLine("Введите цену!");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_products.Supply_id)))
+                errors.AppendLine("Введите ID");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            else
+            {
+                if (_products.Product_id == 0)
+                Entities.GetContext().Products.Add(_products);
+                try
+                {
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно сохранены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
 
         }
+
     }
 }
--- WpfApp8/Pages/ProductPage.xaml.cs
+++ WpfApp8/Pages/ProductPage.xaml.cs
@@ -28,16 +28,47 @@ namespace WpfApp8.Pages
 
         private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new AddEditPage());
+            NavigationService?.Navigate(new Pages.AddEditPage(null));
         }
 
         private void ButtonEdit_OnClick(object sender, RoutedEventArgs e)
         {
-
+            NavigationService?.Navigate(new Pages.AddEditPage((sender as Button).DataContext as Products));
         }
 
         private void ButtonDel_OnClick(object sender, RoutedEventArgs e)
         {
+            var ProdeuctForRemoving = DataGridProduct.SelectedItems.Cast<Products>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {ProdeuctForRemoving.Count()} элементов?", "Внимание",
+                            MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Entities.GetContext().Products.RemoveRange(ProdeuctForRemoving);
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+
+                    DataGridProduct.ItemsSource = Entities.GetContext().Products.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            {
+                if (Visibility == Visibility.Visible)
+                {
+                    Entities.GetContext().ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+                    DataGridProduct.ItemsSource = Entities.GetContext().Products.ToList();
+                }
+            }
 
         }
     }
