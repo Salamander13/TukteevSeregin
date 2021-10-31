3f52bad63e77187b3dc01f0bc6361b05c8494748
SPP User <spp-user@spp-forum.de>
2021-10-31 20:35:14 +0300

    ПР 8

--- WpfApp8/Pages/UserPage.xaml.cs
+++ WpfApp8/Pages/UserPage.xaml.cs
@@ -25,6 +25,54 @@ namespace WpfApp8
             InitializeComponent();
             var currentUsers = Entities.GetContext().User.ToList();
             ListUser.ItemsSource = currentUsers;
+            CmbSorting.SelectedIndex = 0;
+            CheckEmployees.IsChecked = false;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserText.Text = null;
+            CheckEmployees.IsChecked = false;
+            CmbSorting.SelectedIndex = 0;
+        }
+
+        private void UpdateUsers()
+        {
+            //загружаем всех пользователей в список
+            var currentUsers = Entities.GetContext().User.ToList();
+
+            //осуществляем поиск по Ф.И.О. без учета регистра букв
+            currentUsers = currentUsers.Where(x => x.FIO.ToLower().Contains(UserText.Text.ToLower())).ToList();
+
+            //обрабатываем фильтр по одной роли пользователей
+            if (CheckEmployees.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Role.Contains("Продавец")).ToList();
+
+            //осуществляем сортировку в зависимости от выбора пользователя
+            if (CmbSorting.SelectedIndex == 0)
+                ListUser.ItemsSource = currentUsers.OrderBy(x => x.FIO).ToList();
+            else ListUser.ItemsSource = currentUsers.OrderByDescending(x => x.FIO).ToList();
+
         }
     }
 }
